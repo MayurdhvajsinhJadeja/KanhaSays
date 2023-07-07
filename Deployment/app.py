027f15ffd4f4e9345eb139ee678aa7cb68881284
Mayurdhvajsinh Jadeja <mayurdhvajsinhjadeja123@gmail.com>
2023-07-07 14:06:53 +0530

    Update app.py

--- Deployment/app.py
+++ Deployment/app.py
@@ -1,60 +1,48 @@
 import nltk
-
-# nltk.download("punkt")
-# nltk.download("wordnet")
-# nltk.download("popular")
 from nltk.stem import WordNetLemmatizer
 
-nltk.data.path.append("./nltkd/")
+nltk.download("wordnet")
+from nltk.corpus import wordnet
 
 lemmatizer = WordNetLemmatizer()
-import pickle
-import numpy as np
-
-from keras.models import load_model
-
-model = load_model("./model.h5")
-import json
-import random
-
-intents = json.loads(open("./gita_intents.json", encoding="utf-8").read())
-words = pickle.load(open("./texts.pkl", "rb"))
-classes = pickle.load(open("./labels.pkl", "rb"))
 
 
 def clean_up_sentence(sentence):
-    # tokenize the pattern - split words into array
     sentence_words = nltk.word_tokenize(sentence)
-    # stem each word - create short form for word
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
 
-# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+# Use the lemmatizer directly from WordNet corpus
+def lemmatize_word(word):
+    lemma = wordnet._morphy(word, wordnet.NOUN)
+    if lemma is None:
+        lemma = wordnet._morphy(word, wordnet.VERB)
+    if lemma is None:
+        lemma = wordnet._morphy(word, wordnet.ADJ)
+    if lemma is None:
+        lemma = wordnet._morphy(word, wordnet.ADV)
+    if lemma is None:
+        lemma = word
+    return lemma
 
 
 def bow(sentence, words, show_details=True):
-    # tokenize the pattern
     sentence_words = clean_up_sentence(sentence)
-    # bag of words - matrix of N words, vocabulary matrix
     bag = [0] * len(words)
     for s in sentence_words:
         for i, w in enumerate(words):
             if w == s:
-                # assign 1 if current word is in the vocabulary position
                 bag[i] = 1
-                # if show_details:
-                #     print("found in bag: %s" % w)
-    return np.array(bag)
+    return bag
 
 
+# Update the predict_class function to use lemmatize_word
 def predict_class(sentence, model):
-    # filter out predictions below a threshold
     p = bow(sentence, words, show_details=False)
     res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.25
     results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
-    # sort by strength of probability
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
@@ -80,7 +68,6 @@ def chatbot_response(msg):
     ints = predict_class(msg, model)
     res = getResponse(ints, intents)
     return res
-
 
 # s = "how to attain moksha?"
 # print(chatbot_response(s))
